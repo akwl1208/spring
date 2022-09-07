3c291a616fd8df25c20e636c1846e04cb2f3b3cc
Inyo <pageup64@naver.com>
2022-09-07 16:20:09 +0900

    Spring 26일차 : LG (회원)공지사항 목록 보기 구현 완료

--- lg site/lg/src/main/java/kr/green/lg/controller/BoardController.java
+++ lg site/lg/src/main/java/kr/green/lg/controller/BoardController.java
@@ -1,5 +1,7 @@
 package kr.green.lg.controller;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.green.lg.pagenation.Criteria;
+import kr.green.lg.pagenation.PageMaker;
 import kr.green.lg.service.BoardService;
 import kr.green.lg.service.MessageService;
 import kr.green.lg.vo.BoardVO;
@@ -36,11 +40,25 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value = "/board/select", method = RequestMethod.GET)
-	public ModelAndView boardSelect(ModelAndView mv, Integer bd_num) {
+	public ModelAndView boardSelectGet(ModelAndView mv, Integer bd_num) {
 		BoardVO board = boardService.getBoard(bd_num);
 		
 		mv.addObject("bo", board);
 		mv.setViewName("/board/select");
+		return mv;
+	}
+	
+	@RequestMapping(value = "/board/list", method = RequestMethod.GET)
+	public ModelAndView boardListGet(ModelAndView mv, String bd_type, Criteria cri) {
+		ArrayList<BoardVO> boardList = boardService.getBoardList(cri, bd_type);
+		
+		int totalCount = boardService.getTotalCount(cri, bd_type);
+		PageMaker pm = new PageMaker(totalCount, 5, cri);
+		
+		mv.addObject("bd_type", bd_type);
+		mv.addObject("pm", pm);
+		mv.addObject("list", boardList);
+		mv.setViewName("/board/list");
 		return mv;
 	}
 }
